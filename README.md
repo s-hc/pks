2349982a1c9c115a0edf7d46478d235d2badbccf
Samuel Lee <leesamuel423@gmail.com>
2024-02-06 20:49:57 -0500

    skeleton

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 Technologies: Rust, Tauri, Leptos
 
+## Notes
+
+1.Ideally, creating a rust file with an object with all the letters and key press combinations as a key and 0 as value  
+2. When user presses key, the file is accessed and the letter corresponding to the key pressed increments by 1  
+3. When user requests for data aggregation, return heatmap of keyboard presses and statistics and values of key-presses based off of object data
+
+Thinking about project structure:
+`main.rs`: final logic
+`input.rs`: location for data regardingl key logging
+`stats.rs`: locaction for data aggregation and processing (heatmap included here)
+`optimal_setup.rs`: location where aggregated data is utilized to recommend optimal keyboard setup
+
 ## Recommended IDE Setup
 
 [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer).
--- src/app.rs
+++ src/app.rs
@@ -42,25 +42,6 @@ pub fn App() -> impl IntoView {
 
     view! {
         <main class="container">
-            <div class="row">
-                <a href="https://tauri.app" target="_blank">
-                    <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
-                </a>
-                <a href="https://docs.rs/leptos/" target="_blank">
-                    <img src="public/leptos.svg" class="logo leptos" alt="Leptos logo"/>
-                </a>
-            </div>
-
-            <p>"Click on the Tauri and Leptos logos to learn more."</p>
-
-            <p>
-                "Recommended IDE setup: "
-                <a href="https://code.visualstudio.com/" target="_blank">"VS Code"</a>
-                " + "
-                <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank">"Tauri"</a>
-                " + "
-                <a href="https://github.com/rust-lang/rust-analyzer" target="_blank">"rust-analyzer"</a>
-            </p>
 
             <form class="row" on:submit=greet>
                 <input
